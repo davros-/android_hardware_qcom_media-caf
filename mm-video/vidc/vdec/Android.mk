14a091303117c30f6d3d04cb6c648ec87f1252eb
liquid0624 <rjbirnesser@gmail.com>
2013-06-30 22:07:27 -0400

    qcom-media-caf: disable strict aliasing for OMX

--- mm-video/vidc/vdec/Android.mk
+++ mm-video/vidc/vdec/Android.mk
@@ -122,6 +122,7 @@ mm-vdec-test-inc    += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_MODULE                    := mm-vdec-omx-test
 LOCAL_MODULE_TAGS               := debug
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    += -fno-strict-aliasing
 LOCAL_C_INCLUDES                := $(mm-vdec-test-inc)
 
 LOCAL_PRELINK_MODULE      := false
@@ -146,6 +147,7 @@ mm-vdec-drv-test-inc    += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_MODULE                    := mm-video-driver-test
 LOCAL_MODULE_TAGS               := debug
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    += -fno-strict-aliasing
 LOCAL_C_INCLUDES                := $(mm-vdec-drv-test-inc)
 LOCAL_PRELINK_MODULE            := false
 
